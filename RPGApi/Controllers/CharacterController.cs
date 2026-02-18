ab83f7adb15368d1aedbb45d39fa0c44d71a6a02
Radialis <nursultanadilbekov5@gmail.com>
2026-02-18 15:35:15 +0600

    feat: implement CharacterController with basic GET and POST endpoints

---
+++ RPGApi/Controllers/CharacterController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RPGApi.Data;
+using RPGApi.Models;
+
+namespace RPGApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CharacterController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public CharacterController(DataContext context) 
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Character>>> GetAll()
+        {
+            var characters = await _context.Characters.ToListAsync();
+            return Ok(characters);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Character>> Create(Character character)
+        {
+            _context.Characters.Add(character);
+            await _context.SaveChangesAsync();
+
+            return Ok(character);
+        }
+    }
+}
