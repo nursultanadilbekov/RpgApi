8989c341d12af0efb68ba5a8480da241140c5ca0
Radialis <nursultanadilbekov5@gmail.com>
2026-02-22 18:23:13 +0600

    feat: add IAuthService interface for authentication abstraction

---
+++ RPGApi/Services/Interfaces/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using RPGApi.DTOs;
+
+namespace RPGApi.Services.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<string> RegisterAsync(RegisterDto request);
+        Task<string> LoginAsync(LoginDto request);
+    }
+}
