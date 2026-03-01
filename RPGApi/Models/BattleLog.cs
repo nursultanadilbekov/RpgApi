d7b3bb3da888e7fc9ef13835769b561ee7897ed4
Radialis <nursultanadilbekov5@gmail.com>
2026-03-01 11:16:44 +0600

    feat: created BottleLog entity

--- RPGApi/Models/BattleLog.cs
+++ RPGApi/Models/BattleLog.cs
@@ -2,5 +2,18 @@
 {
     public class BattleLog
     {
+        public int Id { get; set; }
+
+        public int CharacterId { get; set; }
+        public Character Character { get; set; }
+
+        public int MonsterId { get; set; }
+        public Monster Monster { get; set; }
+
+        public int Result { get; set; }
+        public int XPGained {  get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
     }
 }
