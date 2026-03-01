f94db8861275bfc66bad1b7ee30b00b86e0dfb56
Radialis <nursultanadilbekov5@gmail.com>
2026-03-01 11:19:07 +0600

    refactor: added BottleLog entity properties

--- RPGApi/Models/Monster.cs
+++ RPGApi/Models/Monster.cs
@@ -1,6 +1,16 @@
-﻿namespace RPGApi.Models
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace RPGApi.Models
 {
     public class Monster
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int MaxHp { get; set; }
+        public int Attack {  get; set; }
+        public int Defence { get; set; }
+
+        public int XPReward { get; set; }
     }
 }
